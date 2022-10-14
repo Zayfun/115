5e1c6a0859fec402938852bb0203d00e1371e449
Zayfun <91322617+Zayfun@users.noreply.github.com>
2022-10-14 07:38:20 +0300

    Add files via upload

---
+++ 11.py
@@ -0,0 +1,24 @@
+print ("operator +", 25+5)
+print ("operator -",25-5)
+print ("operator /",25/5)
+print ("operator *",25*5)
+print ("operator %",15%6)
+print ("operator //",15//6)
+print ("operator **",3**2)
+a=15
+a=a+5
+print ("a=",a)
+a=13
+a-=5
+print ("a=",a)
+
+
+       
+# 1 kg alma 20000 so'm 35 tiyin
+
+a=int(input("so'm:"))
+b=int(input("tiyin:"))
+n=int(input("kg:"))
+c=n*((a*100+b))
+print("so'm:",c//100)
+print("tiyin:",c%100)
